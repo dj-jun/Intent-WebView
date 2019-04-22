73f483bc92146e6d6d512b61a26aaeb2f35ea080
wudongjun <416927096@qq.com>
2019-04-22 17:25:01 +0800

    IntentExample

---
+++ MyBrowser/app/src/main/java/com/example/administrator/mybrowser/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.administrator.mybrowser;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.wv);
+        webView.setWebViewClient(new WebViewClient());
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        if(bundle!=null) {
+            String address=bundle.getString("address");
+            webView.loadUrl(address);
+        }
+    }
+}
